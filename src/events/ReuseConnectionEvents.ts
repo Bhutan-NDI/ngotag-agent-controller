65a7f48711edc85bc0e2a239a369225edba44239
rinkal.bhojani <rinkal.bhojani@ayanworks.com>
2024-10-15 23:11:45 +0530

    debug: added logs for debugging prod issue

--- src/events/ReuseConnectionEvents.ts
+++ src/events/ReuseConnectionEvents.ts
@@ -7,6 +7,7 @@ import { sendWebSocketEvent } from './WebSocketEvents'
 import { sendWebhookEvent } from './WebhookEvent'
 
 export const reuseConnectionEvents = async (agent: Agent, config: ServerConfig) => {
+  console.log('reuseConnectionEvents is getting fired');
   agent.events.on(OutOfBandEventTypes.HandshakeReused, async (event: HandshakeReusedEvent) => {
     const body = {
       ...event.payload.connectionRecord.toJSON(),
@@ -17,11 +18,13 @@ export const reuseConnectionEvents = async (agent: Agent, config: ServerConfig) 
 
     // Only send webhook if webhook url is configured
     if (config.webhookUrl) {
+      console.log(`reuseConnectionEvents is getting fired on ${config.webhookUrl}`);
       await sendWebhookEvent(config.webhookUrl + '/connections', body, agent.config.logger)
     }
 
     if (config.socketServer) {
       // Always emit websocket event to clients (could be 0)
+      console.log(`reuseConnectionEvents is getting fired on socketServer`);
       sendWebSocketEvent(config.socketServer, {
         ...event,
         payload: {
