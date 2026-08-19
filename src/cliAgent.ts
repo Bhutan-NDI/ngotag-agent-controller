558d47277d97c5ec416e79aba2073a42903588b9
devdgna <dev.dgna@gmail.com>
2026-08-19 16:54:57 +0600

    fix: disable agent storage backup on update

--- src/cliAgent.ts
+++ src/cliAgent.ts
@@ -466,6 +466,7 @@ export async function runRestAgent(restConfig: AriesRestConfig) {
     ...afjConfig,
     logger,
     autoUpdateStorageOnStartup: true,
+    backupBeforeStorageUpdate: false,
     // Ideally for testing connection between tenant agent we need to set this to 'true'. Default is 'false'
     // TODO: triage: not sure if we want it to be 'true', as it would mean parallel requests on BW
     // Setting it for now //TODO: check if this is needed
